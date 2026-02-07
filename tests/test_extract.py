391f15d4ccad0aea27bf689a2f8c003c098fd851
Igor Sarnowski <Praca@macbook-pro-igor.home>
2026-02-07 15:47:16 +0100

    test: refactor unit tests for extract to use mock, add some assert and use pytest.fixture to create object once

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -4,44 +4,50 @@ from src.extract.extractor import F1DataExtractor
 from datetime import date
 
 
-def test_get_events_schedule_type_error_year_from():
-    f1_extractor = F1DataExtractor()
+@pytest.fixture
+def extractor():
+    return F1DataExtractor()
+
+
+def test_get_events_schedule_type_error_year_from(extractor):
     with pytest.raises(TypeError, match='Variables should be integers!'):
-        f1_extractor.get_events_schedule(year_from='1234')
+        extractor.get_events_schedule(year_from='1234')
 
 
-def test_get_events_schedule_type_error_year_to():
-    f1_extractor = F1DataExtractor()
+def test_get_events_schedule_type_error_year_to(extractor):
     with pytest.raises(TypeError, match='Variables should be integers!'):
-        f1_extractor.get_events_schedule(year_from=2020, year_to='2020')
+        extractor.get_events_schedule(year_from=2020, year_to='2020')
 
 
-def test_get_events_schedule_value_error_year_from_less_than_2018():
-    f1_extractor = F1DataExtractor()
+def test_get_events_schedule_value_error_year_from_less_than_2018(extractor):
     with pytest.raises(ValueError, match='Year_from value is not correct!'):
-        f1_extractor.get_events_schedule(year_from=2017)
+        extractor.get_events_schedule(year_from=2017)
 
 
-def test_get_events_schedule_value_error_year_from_bigger_than_year_to():
-    f1_extractor = F1DataExtractor()
+def test_get_events_schedule_value_error_year_from_bigger_than_year_to(extractor):
     with pytest.raises(ValueError, match='Year_from value is not correct!'):
         current_year = date.today().year
-        f1_extractor.get_events_schedule(year_from=current_year + 1)
+        extractor.get_events_schedule(year_from=current_year + 1)
 
 
-def test_get_events_schedule_value_error_year_to():
-    f1_extractor = F1DataExtractor()
+def test_get_events_schedule_value_error_year_to(extractor):
     with pytest.raises(ValueError, match='Year_to is greater than current year!'):
         current_year = date.today().year
-        f1_extractor.get_events_schedule(year_from=2019, year_to=current_year + 1)
+        extractor.get_events_schedule(year_from=2019, year_to=current_year + 1)
 
 
-def test_get_events_schedule():
-    f1_extractor = F1DataExtractor()
+def test_get_events_schedule(extractor, mocker):
+    mock_get = mocker.patch('src.extract.extractor.fastf1.get_event_schedule')
 
-    result = f1_extractor.get_events_schedule(year_from=2021, year_to=2021)
+    mock_get.return_value = pd.DataFrame({'RoundNumber': [12, 30, 31],
+                                          'Country': ['Bahrain', 'Italy', 'Portugal']})
+
+    result = extractor.get_events_schedule(year_from=2021, year_to=2021)
 
     assert isinstance(result, pd.DataFrame)
     assert not result.empty
+    assert 'RoundNumber' in result.columns
+    assert 'Country' in result.columns
     assert 'Season' in result.columns
     assert (result['Season'] == 2021).all()
+    assert len(result) == 3
